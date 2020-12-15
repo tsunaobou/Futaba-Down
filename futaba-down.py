706ffe50eaa6f61f3b64dc8cc5ae47e0137491cb
tsunaobou <63852578+tsunaobou@users.noreply.github.com>
2020-12-16 01:20:19 +0900

    機能追加と参考URLの削除

--- futaba-down.py
+++ futaba-down.py
@@ -1,4 +1,4 @@
-#Futaba-down v1.01 
+#Futaba-down v1.02
 
 import requests
 import re
@@ -6,9 +6,10 @@ from bs4 import BeautifulSoup
 import urllib.request
 import os.path
 import os
+from time import sleep
 
 #URL入力
-name = input("ダウンロードしたいスレのURLを入力せよ。")
+name = input("ダウンロードしたいスレのURLを入力:")
 
 #後で使うためにスレのURLを少し加工して持っておく
 name2 = re.match(r'https.*?net',name)
@@ -18,19 +19,20 @@ head = name2.group()
 
 #getリクエスト実行
 response = requests.get(name)
+#3秒スリープ
+print("3秒wait中")
+sleep(3)
 
 #beautifulsoupでHTMLをパース
 soup = BeautifulSoup(response.content,'html.parser')
 
 #後で使うためにtitleタグを抽出
-#参考　https://www.atmarkit.co.jp/ait/articles/1910/18/news015.html
 pagetitle = soup.title.text
 
 #aタグのhref要素を抽出
 geturl = [tag.get('href') for tag in soup('a')]
 
 #被りがあるので重複を削除し再リスト化
-#参考　https://note.nkmk.me/python-list-unique-duplicate/
 duplicate_delete = list(set(geturl))
 
 '''
@@ -44,7 +46,6 @@ rejudge = [redo for redo in duplicate_delete if 'jump.php' not in redo]
 
 #それぞれの拡張子を含むアドレスだけを抽出した配列を６つ作成
 #これは正規表現でやるのが面倒だったからと言う理由もある。
-#参考　https://note.nkmk.me/python-list-str-select-replace/
 jpgget = [feature for feature in rejudge if 'jpg' in feature]
 pngget = [feature for feature in rejudge if 'png' in feature]
 webmget = [feature for feature in rejudge if 'webm' in feature]
@@ -56,21 +57,23 @@ webpget = [feature for feature in rejudge if 'webp' in feature]
 download_list = jpgget+pngget+webmget+webpget+mp4get+gifget
 
 #一つにした配列に最初に作ったアドレスの頭の部分を連結してダウンロード可能なURL形式へ 結果は配列で出てくる
-#参考　https://it-engineer-lab.com/archives/122
 download_ready = list(map(lambda x: head + x,download_list))
 #ダウンロードする個数を数える
 download_number = len(download_list)
 
-#ダウンロード前のフォルダ作成
-#参考　https://note.nkmk.me/python-save-file-at-new-dir/
+#ダウンロード前のフォルダ作成、移動
 os.makedirs(pagetitle,exist_ok=True)
-
+os.chdir(f"{pagetitle}")
 
 #ダウンロードパート
-for url in download_ready:
+for count,url in enumerate(download_ready,1):
 
     filename= os.path.basename(url)
     urllib.request.urlretrieve(url,filename)
+    #進捗状況を表現
+    print(f"{download_number}個中{count}個のダウンロード完了 2秒wait中")
+    #スリープを一回ごとに2sec挟む
+    sleep(2)
 
-#埋め込み参考　https://note.nkmk.me/python-print-basic/
-print('{}個のダウンロードが完了しました！'.format(download_number))
+
+print(f"{download_number}個のメディアのダウンロードが完了しました！")
